ef7eaac65668fad099e35ffb144e3e4c80ba4a8d
“VIOLET-BOSIRE” <violet.bosire@student.moringaschool.com>
2022-12-03 22:31:08 +0300

    assignment completer

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+
+function addingEventListener(input) {
+
+    input = document.getElementById('input');
+    
+    return input.addEventListener("click",()=>alert(""))
+
 }
